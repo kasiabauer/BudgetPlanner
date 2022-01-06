39e6b4e388da19cb5063cfd72ad9969ba6d3e761
bauerka <bauer.kasia@gmail.com>
2022-01-06 16:00:46 +0100

    Add HomePage

--- budgetplanner/budgetplanner_app/views.py
+++ budgetplanner/budgetplanner_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        ctx = {
+            'id': 1,
+            'name': 'name'
+        }
+        return render(request, 'home.html', ctx)
